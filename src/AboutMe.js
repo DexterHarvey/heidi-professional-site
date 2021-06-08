f469c1bd2426f71e7be78c2b8b0839a7135d60f3
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-06-08 17:05:25 -0600

    Added first version of AboutMe component.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,13 +1,25 @@
 import { Component } from 'react';
 import './AboutMe.css'
-import tile_background from './tile_background.png';
+import heidiHeadshot from './heidiHeadshot.jpg';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 export default class AboutMe extends Component {
     render() {
         return (
             <div className='AboutMe'>
-                <img alt='' src={tile_background}></img>
-                <p>heidi about me placeholder</p>
+                <Container>
+                    <Row className='align-items-center' id='AboutMeRow'>
+                        <Col id='AboutMeColHeadshot'xs={12}md={6}>
+                            <Image src={heidiHeadshot} alt='' className='HeidiHeadshot' fluid rounded/>
+                        </Col>
+                        <Col id='AboutMeColDescription' xs={12} md={6}>
+                            <div id='HeidiDescription'>
+                                <h6>Heidi Vander Steen</h6>
+                                <p>Hi my name is Heidi and here is some sample text about me and my life and all the cool things that I have done</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     };

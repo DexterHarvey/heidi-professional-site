0f96408eaed9e31aa81009a947d6a0557709ae86
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-06-09 09:10:18 -0600

    Small styling updates to AboutMe component. Still WIP

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,7 +10,7 @@ export default class AboutMe extends Component {
                 <Container>
                     <Row className='align-items-center' id='AboutMeRow'>
                         <Col id='AboutMeColHeadshot'xs={12}md={6}>
-                            <Image src={heidiHeadshot} alt='' className='HeidiHeadshot' fluid rounded/>
+                            <Image src={heidiHeadshot} alt='' className='HeidiHeadshot' fluid roundedCircle/>
                         </Col>
                         <Col id='AboutMeColDescription' xs={12} md={6}>
                             <div id='HeidiDescription'>

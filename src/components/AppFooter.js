a8e1f04f9a2d8dc169ec8cedf62d7a604ccb0007
Dexter Harvey <74616615+DexterHarvey@users.noreply.github.com>
2021-08-18 08:30:52 -0600

    Refactored AppFooter to be functional

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,24 +2,21 @@ import { Component } from 'react';
 import { Navbar, Container, NavbarBrand, Nav } from 'react-bootstrap';
 import '../styles/AppFooter.scss';
 
-class AppFooter extends Component{
-    render() {
-        return(
-            <div id='footer'>  
-                <Navbar id='navbarFooter'>
-                    <Container>
-                        <NavbarBrand>Heidi Vander Steen</NavbarBrand>
-                        <Navbar.Collapse>
-                            <Nav className='ml-auto'>
-                                <h6>Website by Dexter Harvey</h6>
-                                <h6>DexterHarvey.github.io</h6>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        )
-    }
+function AppFooter() {
+    return(
+        <div id='footer'>  
+            <Navbar id='navbarFooter'>
+                <Container>
+                    <NavbarBrand>Heidi Vander Steen</NavbarBrand>
+                    <Navbar.Collapse>
+                        <Nav className='ml-auto'>
+                            <h6>Website by Dexter Harvey</h6>
+                            <h6>DexterHarvey.github.io</h6>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
-
 export default AppFooter;
